1a1f396186a9154dd59aa23319a0296ae1ce6f72
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-12-13 11:33:11 -0300

    atividade consultas simples

--- aula_02/atividade.sql
+++ aula_02/atividade.sql
@@ -1,52 +1,25 @@
--- Crie uma tabela chamada clientes com as colunas: id, nome, email e telefone, idade.
--- Adicione cinco registros à tabela clientes com informações fictícias, garantindo que cada registro tenha um valor único para o campo id.
--- Crie uma tabela chamada produtos com as colunas: codigo, nome, preco e quantidade.
--- Insira três registros na tabela produtos com informações fictícias.
--- Crie uma tabela chamada itens com as colunas: id, codigo_produto, id_cliente e quantidade. 
--- Adicione cinco itens à tabela itens, relacionando-os a clientes existentes e produtos disponíveis.
+-- Consulta Básica: Escreva uma consulta para selecionar todos os registros da tabela clientes.
+SELECT * FROM clientes;
 
--- CREATE DATABASE atividades;
-USE atividades;
+-- Consulta Básica com Filtro: Recupere o nome e o email dos clientes que têm idade menor que 25 da tabela clientes. 
+SELECT nome, email FROM clientes WHERE idade < 25;
 
--- CREATE TABLE clientes (
---   id INT PRIMARY KEY AUTO_INCREMENT,
---   nome VARCHAR(255) NOT NULL,
---   email VARCHAR(255) NOT NULL,
---   telefone VARCHAR(11),
---   idade INT DEFAULT 10
--- );
+-- Consulta com Filtro: Liste os produtos da tabela produtos onde o preço é menor que 10.0.
+SELECT * FROM produtos WHERE preco < 10;
 
--- INSERT INTO clientes (nome, email, telefone, idade) VALUES
--- ("Almir", "almir@soulcode.com", "00999999999", 23),
--- ("Igor", "igor@soulcode.com", "00999999998", 33),
--- ("Camila", "camila@soulcode.com", "00999999997", 27),
--- ("Carla", "carla@soulcode.com", "00999999996", 27),
--- ("Adriano", "adriano@soulcode.com", "00999999995", 34);
+-- Consulta com Filtro: Liste os itens da tabela itens onde o cliente tenha id 4.
+SELECT * FROM itens WHERE id_cliente = 4;
 
--- CREATE TABLE produtos (
---   codigo VARCHAR(20) PRIMARY KEY,
---   nome VARCHAR(255) NOT NULL,
---   preco FLOAT NOT NULL,
---   quantidade INT NOT NULL DEFAULT 0
--- );
+-- Consulta com Filtro: Liste os itens da tabela itens onde o produto tenha o código 12345.
+SELECT * FROM itens WHERE codigo_produto = "12345";
 
--- INSERT INTO produtos (codigo, nome, preco, quantidade) VALUES
--- ("12345", "Biscoito", 6.99, 23),
--- ("67890", "Melancia", 10.99, 33),
--- ("54321", "Leite", 4.69, 27);
+-- Atualização Simples: Atualize o preço do produto com código 54321 para 29.99 na tabela produtos.
+UPDATE produtos SET preco = 29.99 WHERE codigo = "54321";
 
-CREATE TABLE itens (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  codigo_produto VARCHAR(20) NOT NULL,
-  id_cliente INT NOT NULL,
-  quantidade INT NOT NULL DEFAULT 0,
-  FOREIGN KEY (codigo_produto) REFERENCES produtos(codigo),
-  FOREIGN KEY (id_cliente) REFERENCES clientes(id)
-);
+-- Remoção com Condição: Remova todos os registros da tabela itens onde a quantidade seja igual a zero.
+DELETE FROM itens WHERE quantidade = 0;
 
-INSERT INTO itens (codigo_produto, id_cliente, quantidade) VALUES
-("12345", 1, 2),
-("67890", 2, 1),
-("54321", 3, 1),
-("12345", 4, 2),
-("54321", 5, 2);
+
+-- Atualização com Subconsulta: Incremente em 10% o preço de todos os produtos com nome Biscoito da tabela produtos.
+UPDATE produtos SET preco = preco + (preco * 0.1) WHERE nome = "Biscoito";
+
