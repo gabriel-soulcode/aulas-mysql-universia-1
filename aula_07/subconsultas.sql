a96d96663578e528acb448e696ed6bf6d816ceda
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-12-15 09:21:29 -0300

    subconsultas

---
+++ aula_07/subconsultas.sql
@@ -0,0 +1,6 @@
+USE commerce;
+-- SELECT AVG(preco) AS media_preco FROM produtos;
+-- SELECT * FROM produtos;
+-- SELECT * FROM produtos WHERE preco > (SELECT AVG(preco) FROM produtos);
+-- SELECT * FROM produtos WHERE preco = (SELECT MAX(preco) FROM produtos);
+-- SELECT * FROM produtos WHERE preco = (SELECT MIN(preco) FROM produtos);
